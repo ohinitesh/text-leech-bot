33e8afae598dcf3b30f6c4cf1e19f5d033105c1a
ohinitesh <ohinitesh@gmail.com>
2025-04-14 20:58:38 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -165,11 +165,11 @@ async def download_video(url,cmd, name):
     try:
         if os.path.isfile(name):
             return name
-        elif os.path.isfile(f"{name}.webm"):
-            return f"{name}.webm"
+        elif os.path.isfile(f"{name}.mp4"):
+            return f"{name}.mp4"
         name = name.split(".")[0]
-        if os.path.isfile(f"{name}.mkv"):
-            return f"{name}.mkv"
+        if os.path.isfile(f"{name}.mp4"):
+            return f"{name}.mp4"
         elif os.path.isfile(f"{name}.mp4"):
             return f"{name}.mp4"
         elif os.path.isfile(f"{name}.mp4.webm"):
